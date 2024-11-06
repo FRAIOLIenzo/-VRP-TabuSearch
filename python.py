12cfbfa03efff8f96f5274f3e19d387f66761f6f
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2024-11-06 20:50:34 +0100

    Random function for creating coordonate

--- python.py
+++ python.py
@@ -1,11 +1,25 @@
+import random
+
+
+def generate_coordinates(nb_villes, x_max=100, y_max=100):
+    coordinates = []
+    for _ in range(nb_villes):
+        x = random.randint(1, x_max)
+        y = random.randint(1, y_max)
+        coordinates.append((x, y))
+    return coordinates
+
+
+
+nb_villes = 10
+coordinates = generate_coordinates(nb_villes)
+print(coordinates)
 
 
 
 
 
-
-
-# TODO : faire valeur aléatoire pour représenter les coordonées des villes pour VRP
+# DONE : faire valeur aléatoire pour représenter les coordonées des villes pour VRP
 # TODO : faire algo pour trouver distance entre toutes les villes 
 # TODO : enlever valeur des objets 
 # TODO : enlever capacité du sac car on ne l'utilise pas dans le TSP
