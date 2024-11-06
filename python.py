842177a873e1409283d98758208450df3eceb351
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2024-11-06 11:51:11 +0100

    Refactor code for clarity and remove unnecessary comments

--- python.py
+++ python.py
@@ -10,45 +10,45 @@ def random_objets(poids_max, val_max):
     Renvoie un tuple de 2 dictionnaires (poids,valeur)
     """
     poids_objets =\
-        {i: random.randint(1, poids_max) for i in range(nb_objets)} #SOLUTION
+        {i: random.randint(1, poids_max) for i in range(nb_objets)} 
     valeur_objets =\
-        {i: random.randint(1, val_max) for i in range(nb_objets)}   #SOLUTION
-                                                                    #SOLUTION
-    return poids_objets, valeur_objets                              #SOLUTION
+        {i: random.randint(1, val_max) for i in range(nb_objets)}   
+                                                                    
+    return poids_objets, valeur_objets                              
 
 def random_solution():
-    sac = tuple(random.choice([False, True]) for _ in range(nb_objets)) #SOLUTION
-                                                                        #SOLUTION
-    while (poids_contenu(sac) > capacite):                              #SOLUTION
-        objets_presents = tuple(                                        #SOLUTION
-            i for i, val in enumerate(sac) if val)                      #SOLUTION
-        objet_supprime = random.choice(objets_presents)                 #SOLUTION
-        sac = sac[:objet_supprime] + (False,) + sac[objet_supprime+1:]  #SOLUTION
-                                                                        #SOLUTION
-    return sac                                                          #SOLUTION
+    sac = tuple(random.choice([False, True]) for _ in range(nb_objets)) 
+                                                                        
+    while (poids_contenu(sac) > capacite):                              
+        objets_presents = tuple(                                        
+            i for i, val in enumerate(sac) if val)                      
+        objet_supprime = random.choice(objets_presents)                 
+        sac = sac[:objet_supprime] + (False,) + sac[objet_supprime+1:]  
+                                                                        
+    return sac                                                          
 
 @lru_cache(maxsize=None)
 def poids_contenu(sac):
     """
     Cette fonction renvoie la somme des poids des objets dans le sac
     """
-    return sum(poids_objets[i] for i, val in enumerate(sac) if val) #SOLUTION
+    return sum(poids_objets[i] for i, val in enumerate(sac) if val) 
 
 @lru_cache(maxsize=None)
 def valeur_contenu(sac):
     """
     Cette fonction renvoie la somme des valeurs des objets dans le sac
     """
-    return sum(valeur_objets[i] for i, val in enumerate(sac) if val) #SOLUTION
+    return sum(valeur_objets[i] for i, val in enumerate(sac) if val) 
 
 @lru_cache(maxsize=None)
 def voisinage(sac):
-    voisins = []                                                     #SOLUTION
-    for k in range(len(sac)):                                        #SOLUTION
-        voisin = sac[:k] + (not(sac[k]),) + sac[k+1:]                #SOLUTION
-        if (poids_contenu(voisin) <= capacite):                      #SOLUTION
-            voisins.append(voisin)                                   #SOLUTION
-    return voisins                                                   #SOLUTION
+    voisins = []                                                     
+    for k in range(len(sac)):                                        
+        voisin = sac[:k] + (not(sac[k]),) + sac[k+1:]                
+        if (poids_contenu(voisin) <= capacite):                      
+            voisins.append(voisin)                                   
+    return voisins                                                   
 
 def recherche_tabou(solution_initiale, taille_tabou, iter_max):
     """
@@ -58,45 +58,45 @@ def recherche_tabou(solution_initiale, taille_tabou, iter_max):
        a la liste tabou
     3. On boucle jusqu'a condition de sortie.
     """
-    nb_iter = 0                                                                #SOLUTION
-    liste_tabou = deque((), maxlen = taille_tabou)                             #SOLUTION
-                                                                               #SOLUTION
-    # variables solutions pour la recherche du voisin optimal non tabou        #SOLUTION
-    solution_courante = solution_initiale                                      #SOLUTION
-    meilleure = solution_initiale                                              #SOLUTION
-    meilleure_globale = solution_initiale                                      #SOLUTION
-                                                                               #SOLUTION
-    # variables valeurs pour la recherche du voisin optimal non tabou          #SOLUTION
-    valeur_meilleure = valeur_contenu(solution_initiale)                       #SOLUTION
-    valeur_meilleure_globale = valeur_meilleure                                #SOLUTION
-                                                                               #SOLUTION
-    while (nb_iter < iter_max):                                                #SOLUTION
-        valeur_meilleure = -1                                                  #SOLUTION
-                                                                               #SOLUTION
-        # on parcourt tous les voisins de la solution courante                 #SOLUTION
-        for voisin in voisinage(solution_courante):                            #SOLUTION
-            valeur_voisin=valeur_contenu(voisin)                               #SOLUTION
-                                                                               #SOLUTION
-            # MaJ meilleure solution non taboue trouvée                        #SOLUTION
-            if valeur_voisin > valeur_meilleure and voisin not in liste_tabou: #SOLUTION
-                valeur_meilleure = valeur_voisin                               #SOLUTION
-                meilleure = voisin                                             #SOLUTION
-                                                                               #SOLUTION
-        # on met à jour la meilleure solution rencontrée depuis le début       #SOLUTION
-        if valeur_meilleure > valeur_meilleure_globale:                        #SOLUTION
-            meilleure_globale = meilleure                                      #SOLUTION
-            valeur_meilleure_globale = valeur_meilleure                        #SOLUTION
-            nb_iter = 0                                                        #SOLUTION
-        else:                                                                  #SOLUTION
-            nb_iter += 1                                                       #SOLUTION
-                                                                               #SOLUTION
-        # on passe au meilleur voisin non tabou trouvé                         #SOLUTION
-        solution_courante = meilleure                                          #SOLUTION
-                                                                               #SOLUTION
-        # on met à jour la liste tabou                                         #SOLUTION
-        liste_tabou.append(solution_courante)                                  #SOLUTION
-                                                                               #SOLUTION
-    return meilleure_globale                                                   #SOLUTION
+    nb_iter = 0                                                                
+    liste_tabou = deque((), maxlen = taille_tabou)                             
+                                                                               
+    # variables solutions pour la recherche du voisin optimal non tabou        
+    solution_courante = solution_initiale                                      
+    meilleure = solution_initiale                                              
+    meilleure_globale = solution_initiale                                      
+                                                                               
+    # variables valeurs pour la recherche du voisin optimal non tabou          
+    valeur_meilleure = valeur_contenu(solution_initiale)                       
+    valeur_meilleure_globale = valeur_meilleure                                
+                                                                               
+    while (nb_iter < iter_max):                                                
+        valeur_meilleure = -1                                                  
+                                                                               
+        # on parcourt tous les voisins de la solution courante                 
+        for voisin in voisinage(solution_courante):                            
+            valeur_voisin=valeur_contenu(voisin)                               
+                                                                               
+            # MaJ meilleure solution non taboue trouvée                        
+            if valeur_voisin > valeur_meilleure and voisin not in liste_tabou: 
+                valeur_meilleure = valeur_voisin                               
+                meilleure = voisin                                             
+                                                                               
+        # on met à jour la meilleure solution rencontrée depuis le début       
+        if valeur_meilleure > valeur_meilleure_globale:                        
+            meilleure_globale = meilleure                                      
+            valeur_meilleure_globale = valeur_meilleure                        
+            nb_iter = 0                                                        
+        else:                                                                  
+            nb_iter += 1                                                       
+                                                                               
+        # on passe au meilleur voisin non tabou trouvé                         
+        solution_courante = meilleure                                          
+                                                                               
+        # on met à jour la liste tabou                                         
+        liste_tabou.append(solution_courante)                                  
+                                                                               
+    return meilleure_globale                                                   
 
 nb_objets = 100
 capacite = 20
