04b7e2b872e9cad850c1b7bc224841bd4b9b4de1
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2024-11-07 10:22:28 +0100

    Stats

--- python.py
+++ python.py
@@ -4,6 +4,8 @@ from collections import deque
 import matplotlib.pyplot as plt
 import time
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, value
+import statistics
+
 
 
 
@@ -120,7 +122,7 @@ def recherche_tabou(solution_initiale, taille_tabou, iter_max, matrix):
 start = time.process_time()
 # Main -----------------------------------------------------------------------------------
 print("Main")
-nb_villes = 100
+nb_villes = 10
 
 coordinates = generate_coordinates(nb_villes)
 # print(f"coordinates : {coordinates}")
@@ -142,8 +144,8 @@ random.seed()
 
 
 # Initialisation des paramètres
-taille_tabou = 20
-iter_max = 50
+taille_tabou = 400
+iter_max = 400 
 solution_initiale = path
 tabou, courants, meilleurs_courants = recherche_tabou(solution_initiale, taille_tabou, iter_max, distance_matrix)
 tabou_distance = calculate_path_distance(tabou, distance_matrix)
@@ -160,64 +162,118 @@ plt.legend()
 
 plt.show()
 
-# 271 for 10
-# 1367 for 100
+# 264 for 10
+#Pulp 190
+# 1367 for 100 1317
+#Pulp 566
 
 
+#---------------------------------------------------PULP
 
-def borne_inferieure():
-    villes = range(nb_villes)
+# def borne_inferieure():
+#     villes = range(nb_villes)
 
-    # variables
-    x = LpVariable.dicts('route', ((i,j) for i in villes for j in villes if i!=j), 0, 1)
+#     # variables
+#     x = LpVariable.dicts('route', ((i,j) for i in villes for j in villes if i!=j), 0, 1)
     
-    # probleme
-    prob = LpProblem("tsp_relaxation", LpMinimize)
+#     # probleme
+#     prob = LpProblem("tsp_relaxation", LpMinimize)
 
-    # fonction objective
-    cost = lpSum(distance_matrix[i][j] * x[i,j] for i in villes for j in villes if i!=j)
-    prob += cost
+#     # fonction objective
+#     cost = lpSum(distance_matrix[i][j] * x[i,j] for i in villes for j in villes if i!=j)
+#     prob += cost
 
-    # contraintes
-    for i in villes:
-        prob += lpSum(x[i,j] for j in villes if i!=j) == 1  # chaque ville doit être quittée une fois
-        prob += lpSum(x[j,i] for j in villes if i!=j) == 1  # chaque ville doit être visitée une fois
+#     # contraintes
+#     for i in villes:
+#         prob += lpSum(x[i,j] for j in villes if i!=j) == 1  # chaque ville doit être quittée une fois
+#         prob += lpSum(x[j,i] for j in villes if i!=j) == 1  # chaque ville doit être visitée une fois
 
-    # contrainte additionnelle: revenir à la ville de départ
-    prob += lpSum(x[0,j] for j in villes if j != 0) == 1  # partir de la ville de départ
-    prob += lpSum(x[j,0] for j in villes if j != 0) == 1  # revenir à la ville de départ
+#     # contrainte additionnelle: revenir à la ville de départ
+#     prob += lpSum(x[0,j] for j in villes if j != 0) == 1  # partir de la ville de départ
+#     prob += lpSum(x[j,0] for j in villes if j != 0) == 1  # revenir à la ville de départ
 
-    prob.solve()
-    return value(prob.objective) if (LpStatus[prob.status] == "Optimal") else None
+#     prob.solve()
+#     return value(prob.objective) if (LpStatus[prob.status] == "Optimal") else None
 
-borne = borne_inferieure()
-if borne is not None:
-    print("borne inférieure : ", borne)
-print("valeur de la solution tabou:", str(tabou_distance))
+# borne = borne_inferieure()
+# if borne is not None:
+#     print("borne inférieure : ", borne)
+# print("valeur de la solution tabou:", str(tabou_distance))
 
 #-------------------------------------------------------------------MULTI-START
 
-taille_tabou = 60
-iter_max = 10
+# taille_tabou = 60
+# iter_max = 10
 
-# multi-start de 500 itérations
-val_max = float('inf')
-sol_max = None
+# # multi-start de 500 itérations
+# val_max = float('inf')
+# sol_max = None
 
-sac = solution_initiale
-for _ in range(10):
+# sac = solution_initiale
+# for _ in range(10):
 
-    print("sac = " + str(sac))
-    print("valeur initiale = " + str(calculate_path_distance(sac, distance_matrix)))
+#     print("sac = " + str(sac))
+#     print("valeur initiale = " + str(calculate_path_distance(sac, distance_matrix)))
 
-    sol_courante, _, _ = recherche_tabou(sac, taille_tabou, iter_max, distance_matrix)
-    val_courante = calculate_path_distance(sol_courante, distance_matrix)
-    print("valeur courante = " + str(val_courante))
+#     sol_courante, _, _ = recherche_tabou(sac, taille_tabou, iter_max, distance_matrix)
+#     val_courante = calculate_path_distance(sol_courante, distance_matrix)
+#     print("valeur courante = " + str(val_courante))
     
-    if val_courante < val_max:
-        print("nouveau max = " + str(val_courante))
-        val_max = val_courante
-        sol_max = sol_courante
-    sac = generate_path(nb_villes, 0)
+#     if val_courante < val_max:
+#         print("nouveau max = " + str(val_courante))
+#         val_max = val_courante
+#         sol_max = sol_courante
+#     sac = generate_path(nb_villes, 0)
 
-print("valeur finale = " + str(val_max))
+# print("valeur finale = " + str(val_max))
+
+#---------------------------------------------------------------Statistique
+
+
+# paramètres du test
+tabou_min = 1
+tabou_max = 200
+nb_villes = 10
+
+nb_test = 100
+iter_max = 20
+
+# pour stocker les résultats
+moyennes = []
+deviations = []
+
+random.seed(9)
+
+
+# génération aléatoire de l'instance 
+coordinates = generate_coordinates(nb_villes)
+distances_dict = calculate_distances(coordinates)
+distance_matrix = distances_to_matrix(distances_dict, nb_villes)
+
+# boucle sur la taille de la liste tabou
+for taille_tabou in range(tabou_min, tabou_max):
+    distances = deque(())
+    for _ in range(nb_test):
+
+        
+        path = generate_path(nb_villes, 0)
+        solution, _, _ = recherche_tabou(path, taille_tabou, iter_max, distance_matrix)
+        val = calculate_path_distance(solution, distance_matrix)
+        distances.append(val)
+        
+                                                            
+    moyennes.append(statistics.mean(distances))
+    deviations.append(statistics.stdev(distances))
+
+# affichage de la courbe de moyenne
+plt.plot(range(tabou_min, tabou_max), moyennes)
+
+# affichage de la bande d'écart-type
+plt.fill_between(range(tabou_min, tabou_max),
+                 [m - d for m, d in zip(moyennes, deviations)],
+                 [m + d for m, d in zip(moyennes, deviations)],
+                 alpha=.1)
+plt.xlabel("taille de la liste tabou")
+plt.ylabel("distance du parcours")
+plt.title("Impact de la taille de la liste tabou sur la qualité des solutions")
+plt.show()
