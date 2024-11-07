0ac3d1d9c95aa7d0e6ab62d0d09dd087a92ccc52
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2024-11-07 08:55:46 +0100

    Borne inférieure Pulp

--- python.py
+++ python.py
@@ -4,6 +4,7 @@ from collections import deque
 from functools import lru_cache
 import matplotlib.pyplot as plt
 import time
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, value
 
 random.seed(10)
 def generate_coordinates(nb_villes, x_max=100, y_max=100):
@@ -116,22 +117,28 @@ def recherche_tabou(solution_initiale, taille_tabou, iter_max, matrix):
 start = time.process_time()
 # Main -----------------------------------------------------------------------------------
 print("Main")
-nb_villes = 50
+nb_villes = 100
 
 coordinates = generate_coordinates(nb_villes)
 # print(f"coordinates : {coordinates}")
+# plt.scatter(*zip(*coordinates.values()))
+# plt.title("City Coordinates")
+# plt.xlabel("X Coordinate")
+# plt.ylabel("Y Coordinate")
+# plt.show()
+
 distances = calculate_distances(coordinates)
-# print(f"distances : {distances}")
+
 distance_matrix = distances_to_matrix(distances, nb_villes)
 # for row in distance_matrix:
 #     print(" ".join(f"{dist:3}" for dist in row))
 path = generate_path(nb_villes, 0)
-# print(f"path : {path}")
+
 
 
 # Initialisation des paramètres
 taille_tabou = 10
-iter_max = 50
+iter_max = 10
 solution_initiale = path
 tabou, courants, meilleurs_courants = recherche_tabou(solution_initiale, taille_tabou, iter_max, distance_matrix)
 tabou_distance = calculate_path_distance(tabou, distance_matrix)
@@ -150,3 +157,31 @@ plt.show()
 
 # 271 for 10
 # 1367 for 100
+
+
+
+def borne_inferieure():
+    villes = range(nb_villes)
+
+    # variables
+    x = LpVariable.dicts('route', ((i,j) for i in villes for j in villes if i!=j), 0, 1)
+    
+    # probleme
+    prob = LpProblem("tsp_relaxation", LpMinimize)
+
+    # fonction objective
+    cost = lpSum(distance_matrix[i][j] * x[i,j] for i in villes for j in villes if i!=j)
+    prob += cost
+
+    # contraintes
+    for i in villes:
+        prob += lpSum(x[i,j] for j in villes if i!=j) == 1  # chaque ville doit être quittée une fois
+        prob += lpSum(x[j,i] for j in villes if i!=j) == 1  # chaque ville doit être visitée une fois
+
+    prob.solve()
+    return value(prob.objective) if (LpStatus[prob.status] == "Optimal") else None
+
+borne = borne_inferieure()
+if borne is not None:
+    print("borne inférieure : ", borne)
+print("valeur de la solution tabou:", str(tabou_distance))
